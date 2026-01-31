250493fcb7fa0e387bb9bf5b79c26083fdff8aa6
Marcello Targa <121225428+targamarcello@users.noreply.github.com>
2026-01-31 08:31:28 +0100

    sistemata pseudoApp

--- pseudoApp/iscrizioneCorsi.php
+++ pseudoApp/iscrizioneCorsi.php
@@ -32,6 +32,7 @@ $professori = [
 ];
 $corsi = ["sistemi e reti", "info", "statistica", "contabilità", "marketing", "tecnologie", "meccatronica", "elettronica", "chimica organica", "robotica"];
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -39,15 +40,41 @@ $corsi = ["sistemi e reti", "info", "statistica", "contabilità", "marketing", "
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Adesione Corsi</title>
 </head>
 <body>
 <form method="post" action="tabellaCorsi.php">
-    <!--Stampa inserimento corsi-->
-    <?php for ($i =0; $i<$nCorsi;$i++){?>
+    <?php for ($i = 0; $i <= $nCorsi; $i++): ?>
+        <div class="box">
+            <h3>Corso <?= $i ?></h3>
+            <div class="campo">
+                <label>Professore:</label><br>
+                <label>
+                    <select name="professore[<?= $i ?>]">
+                        <?php foreach ($professori as $prof => $key): ?>
+                            <option value="<?= $prof ?>">
+                                <?= $key['nome'] . " - " . $key['cognome'] ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </label>
+            </div>
 
-    <?php }?>
+            <div class="campo">
+                <label>Corsi:</label><br>
+                <select name="corsi[<?= $i ?>][]">
+                    <?php foreach ($corsi as $corso): ?>
+                        <option value="<?= $corso ?>">
+                            <?= ucfirst($corso) ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
 
+        </div>
+    <?php endfor; ?>
+    <button type="submit">Invia</button>
 </form>
 </body>
 </html>
--- pseudoApp/tabellaCorsi.php
+++ pseudoApp/tabellaCorsi.php
@@ -1,5 +1,8 @@
 <?php
+$professori = $_POST["professori"]??[];
+$corsi = $_POST["corsi"]??[];
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,9 +10,30 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iscrizione Corsi</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Tabella Corsi</title>
 </head>
-<body>
 
+<body>
+<h2>Adesione Corsi</h2>
+<table>
+    <tr>
+        <th>Nome</th>
+        <th>Cognome</th>
+        <th>Corso/i</th>
+    </tr>
+
+    <?php foreach ($professori as $prof => $key): ?>
+        <?php
+            list($nome, $cognome) = explode(";", $key);
+            $listaCorsi = $corsi[array_search($corsi)[$key]] ?? [];
+        ?>
+        <tr>
+            <td><?= $nome?></td>
+            <td><?= $cognome?></td>
+            <td><?= implode(',',$listaCorsi)?></td>
+        </tr>
+    <?php endforeach ?>
+</table>
 </body>
 </html>
