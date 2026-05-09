dddf3f89ea08bd60f8972171a6d9da280c633ebd
Marcello Targa <121225428+targamarcello@users.noreply.github.com>
2026-05-09 11:28:12 +0200

    progetto da studiare
    progetto da studiare di mvc

---
+++ mvc26/Config/databaseConfig.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'dns'=>'mysql:host=192.168.60.144;dbname=marcello_targa_tablet_store',
+    'username' => 'marcello_targa',
+    'password' => 'contato.raffermi.',
+    'options' => [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+];
